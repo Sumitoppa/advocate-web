0548636434cf983e993f88da4c819be398a4997f
SuryaChakraborty <70584786+SuryaChakraborty@users.noreply.github.com>
2024-01-03 00:06:44 +0530

    check commit

--- src/components/Homepage/MissionAndVision/MissionAndVision.jsx
+++ src/components/Homepage/MissionAndVision/MissionAndVision.jsx
@@ -15,7 +15,7 @@ function MissionAndVission() {
               </Typography>
               <Typography className="card-text">
                 Some quick example text to build on the card title and make up
-                the bulk of the card's content. Test
+                the bulk of the card's content.
               </Typography>
             </div>
           </div>
